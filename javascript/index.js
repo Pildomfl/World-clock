f3c0294df1bc17adb49fcd0eb095dd31beb75ec3
Pildomfl <151389400+Pildomfl@users.noreply.github.com>
2023-12-13 16:05:11 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,7 @@ function updateTime() {
   let losAngelesElement = document.querySelector("#los-angeles");
   if (losAngelesElement) {
     let losAngelesDateElement = losAngelesElement.querySelector(".date");
-    let lostAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
     let losAngelesTime = moment().tz("America/Los_Angeles");
 
     losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
